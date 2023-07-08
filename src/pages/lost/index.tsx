c30863a6ee42b1941454159d16cd71c9040959f7
kir4che <mollydcxxiii@gmail.com>
2023-07-08 12:27:19 +0800

    Update index.tsx

--- src/pages/lost/index.tsx
+++ src/pages/lost/index.tsx
@@ -17,13 +17,13 @@ export default function Lost() {
   }
 
   return (
-    <div className="w-full px-10 pt-4 pb-20 mx-auto">
+    <div className="w-full pt-4 pb-20 mx-auto">
       <AlertInfo info={'資料來源於行政院農委會的動物認領養 API，尋獲及領回貓咪的飼主，記得至登記站辦理登記～'} />
-      <div className="flex flex-row flex-wrap pt-12">
-        <aside className="hidden lg:block lg:w-1/5">
+      <div className="flex pt-12 sm:space-x-12 lg:space-x-24">
+        <aside className="hidden md:block">
           <LostFilter onFilterChange={handleFilterChange} />
         </aside>
-        <main className="lg:w-4/5">
+        <main>
           <LostPost {...filterValues} />
         </main>
       </div >
